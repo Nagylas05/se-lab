5d80d6a85bfd0039163386d5c6e6b6be57010ce4
unknown <185249@BME.HU>
2024-10-09 09:33:40 +0200

    negyedik

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -11,7 +11,7 @@ public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
-  private Random generator = new Random();
+  private Random generator = new Random(); //kösz
 
   private int torpedoCount = 0;
 
@@ -42,7 +42,7 @@ public class TorpedoStore {
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount -= numberOfTorpedos;
+      this.torpedoCount -= numberOfTorpedos; //mert szintaktikailag ez így a helyes.
       success = true;
     } else {
       // simulated failure
